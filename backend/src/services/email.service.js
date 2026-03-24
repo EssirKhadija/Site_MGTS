980c9e7b9fe06a88f6b662ca46b421df4e8fadea
Webigit <mohamedamineasrir@gmail.com>
2026-03-24 18:53:24 +0000

    fix email services

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -7,6 +7,10 @@ const transporter = nodemailer.createTransport({
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  family: 4,
 });
 
 const sendVerificationEmail = async (email, fullName, token) => {
